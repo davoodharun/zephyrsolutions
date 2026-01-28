878634950718e2fcd5fd27301116ac00398d95dc
Harun Davood <davoodharun@gmail.com>
2026-01-28 10:51:50 -0500

    updates

---
+++ functions/_worker.ts
@@ -0,0 +1,12 @@
+/**
+ * Cloudflare Pages Functions entry point
+ * This file helps ensure Cloudflare detects this as a Functions project
+ * 
+ * Note: Cloudflare Pages automatically detects functions/ directory,
+ * but this file can help with detection in some cases.
+ */
+
+// This is a marker file to help Cloudflare detect Functions
+// The actual functions are in api/ subdirectories
+
+export {};
